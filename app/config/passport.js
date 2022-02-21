02000b3c8cfde35e3dcd7020cdab49528dd69284
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-02-21 16:54:55 +0530

    Successfully implemented passport-jwt authentication and protected routes with passport

---
+++ app/config/passport.js
@@ -0,0 +1,23 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/User')
+const passport = require('passport')
+
+const opts = {}
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.JWT_SECRET;
+
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.id }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
+
