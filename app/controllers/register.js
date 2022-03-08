fe8f785474c0c336c1f1fe00e97cc8948916637f
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-08 23:58:33 +0530

    fixed a bug /register route

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -8,27 +8,29 @@ exports.register = async (req, res) => {
     if (checkEsistence) {
         res.status(200).json({ message: "User already exists" })
     }
-    const user = new User({
-        email: email,
-        password: hashSync(password, 10),
-    })
-    user.save().then(user => {
-        res.status(200).json({
-            success: true,
-            message: "User created successfully.",
-            user: {
-                id: user._id,
-                email: user.email
-            }
+    else {
+        const user = new User({
+            email: email,
+            password: hashSync(password, 10),
         })
+        user.save().then(user => {
+            res.status(200).json({
+                success: true,
+                message: "User created successfully.",
+                user: {
+                    id: user._id,
+                    email: user.email
+                }
+            })
 
-    }).catch(err => {
-        res.send({
-            success: false,
-            message: "Something went wrong",
-            error: err
+        }).catch(err => {
+            res.send({
+                success: false,
+                message: "Something went wrong",
+                error: err
+            })
         })
-    })
+    }
 }
 
 
