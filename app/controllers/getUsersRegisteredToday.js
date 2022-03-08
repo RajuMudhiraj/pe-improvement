07a8604d01581dd4379ad5f31123e9a7d2ffdfbe
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-08 23:42:44 +0530

    added pagination for /getUsersRegisteredToday route

--- app/controllers/getUsersRegisteredToday.js
+++ app/controllers/getUsersRegisteredToday.js
@@ -3,14 +3,27 @@ const User = require('../models/User')
 
 // Get all users 
 exports.getUsersRegisteredToday = async (req, res) => {
+    const { page, limit } = req.query;
+
     const date = new Date()
     try {
         let users = await User.find({
             createdAt: {
                 $gte: new Date(`"${date.getFullYear()}, ${date.getMonth() + 1}, ${date.getDate()}"`)
             }
-        });
-        res.status(200).json({ count: users.length })
+        })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec();
+
+        const count = await User.find({ createdAt: { $gte: new Date(`"${date.getFullYear()}, ${date.getMonth() + 1}, ${date.getDate()}"`) } });
+        const totalCount = count.length;
+
+        res.status(200).json({
+            users,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: Number(page)
+        })
     }
     catch (err) {
         console.log(err)
