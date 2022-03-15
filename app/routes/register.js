7f8f7dfb50aed6a0f77dae08cf63f7a246a18e15
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-15 23:10:38 +0530

    added swaggerUI for post method of  /register route

--- app/routes/register.js
+++ app/routes/register.js
@@ -7,6 +7,60 @@ const {
     register,
 } = require('../controllers/register')
 
+// Swagger documentation for User Schema.
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: The email Id
+ *         password:
+ *           type: string
+ *           description: The password
+ *         
+ *       example:
+ *         email: test@gmail.com
+ *         password: password
+ *                
+ */
+
+// Todos tag for grouping all User managing apis
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: The User managing apis
+ */
+
+
+/**
+* @swagger
+* /register:
+*   post:
+*     summary: Create new user
+*     tags: [User]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The response of new user registered
+*         content:
+*           application/json:
+*             schema:
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.post('/', register);
 
 
