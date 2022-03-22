087bb8e24e221ec333f1c1ae041f50b3a9b82473
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-22 14:37:16 +0530

    Added swaggerUI for get method of /userActivity/thisWeek api end point

--- app/routes/userActivity.js
+++ app/routes/userActivity.js
@@ -11,7 +11,7 @@ const {
 
 // routes for 'admin' role
 
-// Swagger documentation for get method of /todo/{id} .
+// Swagger documentation for get method of /userActivity/thisDay
 /**
  * @swagger
  * /userActivity/thisDay:
@@ -41,6 +41,36 @@ const {
  *               message: Error message
  */
 router.get('/thisDay', passport.authenticate('jwt', { session: false }), thisDay);
+
+// Swagger documentation for get method of /userActivity/thisWeek
+/**
+ * @swagger
+ * /userActivity/thisWeek:
+ *   get:
+ *     summary: Returns a list of this week's active users.
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: this week's active users
+ *         content:
+ *           application/json:
+ *             example:
+ *               -
+ *                 _id: 621600c9d18d6205ec5aa14a
+ *                 email: "email@gmail.com"
+ *                 createdAt: 2022-02-23T09:39:21.641Z
+ *                 updatedAt: 2022-02-23T09:39:21.641Z
+ *                 __v: 0
+ *       400:
+ *         description: Error message.
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Error message
+ */
 router.get('/thisWeek', passport.authenticate('jwt', { session: false }), thisWeek);
 router.get('/thisMonth', passport.authenticate('jwt', { session: false }), thisMonth);
 
