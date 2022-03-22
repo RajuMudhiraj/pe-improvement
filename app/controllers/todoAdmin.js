be6666403189765de3b118e2488967418db17bfa
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-22 12:09:24 +0530

    Implemented queries based search for /todoAdmin route

--- app/controllers/todoAdmin.js
+++ app/controllers/todoAdmin.js
@@ -3,40 +3,30 @@ const _ = require('lodash')
 
 // Get all users Todos
 exports.getAllUsersTodos = async (req, res) => {
-    const { searchByTitle, searchByCategory, sortByCreatedAt } = req.query;
-    console.log(req.user)
+    const { searchByTitle, searchByCategory, sortByCreatedAt, limit, page } = req.query;
     try {
-        let todo = await Todo.find();
-        // SearchByTitle
-        if (searchByTitle) {
-            let filteredByTitle = todo.filter(a => _.toLower(a.title.replace(/ /g, "")) == _.toLower(searchByTitle.replace(/ /g, "")));
-            res.status(200).json(filteredByTitle)
-        }
-        // Search By Category
-        else if (searchByCategory) {
-            let filteredByCategory = todo.filter(a => a.category.includes(searchByCategory));
-            res.status(200).json(filteredByCategory)
-        }
-        // Sort By CreatedAt Asc
-        else if (sortByCreatedAt) {
-            if (sortByCreatedAt == "asc") {
-                todo.sort((a, b) => { return new Date(a.createdAt) - new Date(b.createdAt) })
-                res.status(200).json(todo)
-            }
-            // Sort By CreatedAt Desc
+        // find all todos of a user
+        let todo = await Todo.find({ title: { $regex: new RegExp(searchByTitle, "i") }, category: { $regex: new RegExp(searchByCategory, "i") } })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec();
 
-            else if (sortByCreatedAt == "desc") {
-                todo.sort((a, b) => { return new Date(b.createdAt) - new Date(a.createdAt) })
-                res.status(200).json(todo)
-            }
-        }
-        else {
-            res.status(200).json(todo)
-        }
+        // fetching all todos of a user to get actual count of todos
+        const count = await Todo.find({ title: { $regex: new RegExp(searchByTitle, "i") }, category: { $regex: new RegExp(searchByCategory, "i") } });
+        const totalCount = count.length;
+
+        res.status(200).json({
+            todo,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: Number(page)
+        })
 
     }
     catch (err) {
-        res.status(400).json(`Error:"Test", ${err}`)
+        res.status(400).json({
+            success: false,
+            message: err + ""
+        })
     }
 
 }
